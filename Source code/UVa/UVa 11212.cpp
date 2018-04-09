2cbe1f5a78a29287da535733f374e3a0ab17a0c3
DinnerHunt <DinnerHunt@users.noreply.github.com>
2018-04-10 07:50:23 +0800

    2018.4.10 7.45
    UVa 11212
    UVa 12325

--- Source code/UVa/UVa 11212.cpp
+++ Source code/UVa/UVa 11212.cpp
@@ -1,53 +1,66 @@
-#include <cstdio>
-#include <cstring>
+// UVa11212 Editing a Book
+#include<cstdio>
+#include<cstring>
 using namespace std;
-int a[10],n,maxn;
-int h(){
-	int x=0;
-	for(int i=0;i<n-1;i++)
-		if(a[i]+1!=a[i+1]) x++;
-	if(a[n-1]!=n) x++;
-	return x;
-}
-bool ok(){
-	for(int i=0;i<n-1;i++)
-		if(a[i]>=a[i+1]) return false;
-	return true;
-}
-bool dfs(int x){
-	if(x*3+h() > maxn*3) return false;
-	if(ok())return true;
 
-	int b[10],c[10];
-	memcpy(c,a,sizeof(a));
-	for(int i=0;i<n;i++)
-		for(int j=i;j<n;j++)
-		{
-			int cnt=0;
-			for(int k=0;k<n;k++)
-				if(k<i||k>j) b[cnt++]=a[k];  //保留不切部分
-			for(int k=0;k<=cnt;k++){
-				int cnt2 = 0;
-				for(int p=0;p<k;p++) a[cnt2++] = b[p];
-				for(int p=i;p<=j;p++) a[cnt2++] = c[p];
-				for(int p=k;p<cnt;p++) a[cnt2++] = b[p];
-				if(dfs(x+1)) return true;
-				memcpy(a,c,sizeof(a));
-			}
-		}
-	return false;
+const int maxn = 9;
+int n, a[maxn];
+
+bool is_sorted() {
+  for(int i = 0; i < n-1; i++)
+    if(a[i] >= a[i+1]) return false;
+  return true;
 }
-int main(){
-	int Case=0;
-	while(scanf("%d",&n)==1&&n){
-		for(int i=0;i<n;i++)
-			scanf("%d",&a[i]);
-		for(maxn = 0;maxn < 5; maxn++){
-			if(!maxn) if(ok())break;
-			if(dfs(0)) break;
-		}
-		if(Case)printf("\n");
-		printf("Case %d: %d",++Case,maxn);
-	}
-	return 0;
+
+// the number of integers with incorrect successor
+int h() {
+  int cnt = 0;
+  for(int i = 0; i < n-1; i++)
+    if(a[i]+1 != a[i+1]) cnt++;
+  if(a[n-1] != n) cnt++;
+  return cnt;
+}
+
+bool dfs(int d, int maxd) {
+  if(d*3 + h() > maxd*3) return false;
+  if(is_sorted()) return true;
+
+  int b[maxn], olda[maxn];
+  memcpy(olda, a, sizeof(a));
+  for(int i = 0; i < n; i++)
+   for(int j = i; j < n; j++) {
+     // cut
+     int cnt = 0;
+     for(int k = 0; k < n; k++)
+       if(k < i || k > j) b[cnt++] = a[k];
+
+     // insert before position k
+     for(int k = 0; k <= cnt; k++) {
+       int cnt2 = 0;
+       for(int p = 0; p < k; p++) a[cnt2++] = b[p];
+       for(int p = i; p <= j; p++) a[cnt2++] = olda[p];
+       for(int p = k; p < cnt; p++) a[cnt2++] = b[p];
+
+       if(dfs(d+1, maxd)) return true;
+       memcpy(a, olda, sizeof(a));
+     }
+   }
+  return false;
+}
+
+int solve() {
+  if(is_sorted()) return 0;
+  int max_ans = 5; // after experiments, we found ans <= 5 for n <= 9
+  for(int maxd = 1; maxd < max_ans; maxd++)
+    if(dfs(0, maxd)) return maxd;
+  return max_ans;
+}
+
+int main() {
+  int kase = 0;
+  while(scanf("%d", &n) == 1 && n) {
+    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
+    printf("Case %d: %d\n", ++kase, solve());
+  }
+  return 0;
 }
